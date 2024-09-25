b9c589009e152eba8fee5e97bb8e6060fdf0fe24
ChurroDevelop <jhonfredy2325@gmail.com>
2024-09-25 12:44:17 -0500

    ola:)

--- resources/views/centros/partials/form.blade.php
+++ resources/views/centros/partials/form.blade.php
@@ -1,9 +1,29 @@
-
-{{ html()->label('nombre') }}
-{{ html()->text('nombre') }}
-{{ html()->label('direccion') }}
-{{ html()->text('direccion') }}
-{{ html()->label('telefono_centro') }}
-{{ html()->number('telefono_centro') }}
-{{ html()->hidden('regional_id')->value($regional->id) }}
-
+<div class="w-full p-8 flex flex-col gap-4">
+    <div class="flex flex-col gap-2">
+        <div>
+            {{ html()->label('Nombre del centro')->class('font-bold') }}
+        </div>
+        <div>
+            {{ html()->text('nombre')->class('input input-bordered input-sm w-full')->placeholder('Ingrese el nombre del centro') }}
+        </div>
+    </div>
+    <div class="flex flex-col gap-4">
+        <div>
+            {{ html()->label('Direccion del centro')->class('font-bold') }}
+        </div>
+        <div>
+            {{ html()->text('direccion')->class('input input-bordered input-sm w-full')->placeholder('Ingrese la direccion del centro') }}
+        </div>
+    </div>
+    <div class="flex flex-col gap-4">
+        <div>
+            {{ html()->label('Telefono del centro')->class('font-bold') }}
+        </div>
+        <div>
+            {{ html()->number('telefono_centro')->class('input input-bordered input-sm w-full')->placeholder('Ingrese el telefono del centro') }}
+        </div>
+    </div>
+    <div>
+        {{ html()->hidden('regional_id')->value($regional->id) }}
+    </div>
+</div>
